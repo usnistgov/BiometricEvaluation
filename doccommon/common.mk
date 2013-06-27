787f7ba1487cef60379a22285a6dacf5dc3275fd
Greg Fiumara <gregory.fiumara@nist.gov>
2013-06-27 14:48:10 -0500

    Improvements to a universal LaTeX build system.  Add support for multi-target builds where doxygen documentation is different, migrating FPVTE2012 as an example.  Clean and document some.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/": change = 5013]

--- doccommon/common.mk
+++ doccommon/common.mk
@@ -7,9 +7,36 @@
 # about its quality, reliability, or any other characteristic.
 
 #
+# To create a Makefile for a LaTeX document:
+#     - Define the following make variables:
+#	- ROOTNAME: Name of the document
+#	- SOURCETEX: TeX files that comprise the document
+#	- LOCALINC: Relative path to 'doccommon'
+#	- .DEFAULT_GOAL: make goal that calls thedoc
+#
+#     - Create a make goal that makes 'thedoc'
+#	.DEFAULT_GOAL := all
+#	- all:
+#		$(MAKE) thedoc
+#
+#     - If arguments to pdflatex are needed, append "-pdflatex="pdflatex <args>"
+#	to $(LATEXMK)
+#
+# To add doxygen documentation:
+#     - Listthe source code to include in SOURCECODE make variable
+#
+#     - include doccommon/common.mk
+#
+#     - If doxygen is to be run and the doxygen configuration file is not 
+#	$(ROOTNAME).tex, define DOXYCONFIG and set GENERATEDOXYGEN to YES
+#
+# Good luck.
+#
+
+#
 # Override executables
 #
-LATEXMK = $(shell which latexmk) -pdf 
+LATEXMK = $(shell which latexmk) -pdf -recorder
 DOXYGEN = $(shell which doxygen)
 
 #
@@ -25,8 +52,11 @@ GENERATEDOXYGEN = $(shell test -f $(DOXYCONFIG) && echo YES || echo NO)
 GLOSSARY = glossary.tex
 GENERATEGLOSSARY = $(shell test -f $(GLOSSARY) && echo YES || echo NO)
 
+# Assets directory (local and shared)
+ASSETS = assets
+
 # Common Images
-SOURCEIMAGES = $(LOCALINC)/assets/*
+SOURCEIMAGES = $(LOCALINC)/$(ASSETS)/*
 
 # Common Bibliography
 COMMONBIB = $(LOCALINC)/common.bib
@@ -48,7 +78,6 @@ LATEXBIB = $(LATEXROOTNAME).bib
 LATEXMAIN = $(ROOTNAME).tex
 PDFMAIN = $(ROOTNAME).pdf
 
-
 #
 # Check whether the main PDF file exists in the top-level doc directory, and
 # whether it's writable. If not present, we'll just create it; if present, and
@@ -63,10 +92,34 @@ else
 	PDFMAINWRITABLE = yes
 endif
 
-all: build $(PDFMAIN) 
+thedoc: build $(PDFMAIN)
+# Error if main Makefile does not define a default goal
+ifeq ($(.DEFAULT_GOAL), )
+	$(error DEFAULT_GOAL is not set)
+endif
+
 build:
+# Make directory structure
 	mkdir -p $(LATEXBUILDDIR)
 	mkdir -p $(HTMLBUILDDIR)
+# Combined project-level bibliography files with the common references
+	cat $(SOURCEBIB) $(LOCALINC)/shared.bib > $(LATEXBUILDDIR)/$(LATEXBIB)
+ifneq ($(SOURCEBIB), $(LATEXBIB))
+	ln -s $(LATEXBIB) $(LATEXBUILDDIR)/$(SOURCEBIB)
+endif
+	cp -f $(SOURCETEX) $(LATEXBUILDDIR)
+# Copy any images into the build directory
+	cp -f $(SOURCEIMAGES) $(LATEXBUILDDIR)
+# Add rule to generate glossary, if present
+ifeq ($(GENERATEGLOSSARY), YES)
+	cp -f $(GLOSSARY) $(LATEXBUILDDIR)
+	ln -s $(GLOSSARY) $(LATEXBUILDDIR)/$(basename $GLOSSARY).glo
+	cp $(LOCALINC)/latexmkrc $(LATEXBUILDDIR)
+endif
+# Copy local assets, if present
+	if [ -d $(ASSETS) ]; then					\
+		cp -r -f $(ASSETS)/* $(LATEXBUILDDIR);			\
+	fi
 
 $(PDFMAIN): $(LATEXBUILDDIR)/$(LATEXPDF)
 ifneq ($(PDFMAINWRITABLE),yes)
@@ -75,39 +128,21 @@ else
 	cp -p $(LATEXBUILDDIR)/$(LATEXPDF) $(PDFMAIN)
 endif
 
-#
-# The LaTeX build is a bit complicated, because Doxygen names the
-# LaTeX related files to start with 'refman'. In order to keep the
-# bibliography file named based on our document name, it is copied
-# into the build directory as refman.bib, but linked to the name we
-# want.
-# Also copy the non-Doxygen generated source files to the build directory.
-#
+# Build relies on doxygen config, if present
 ifeq ($(GENERATEDOXYGEN), YES)
 $(LATEXBUILDDIR)/$(LATEXPDF): $(SOURCECODE) $(SOURCETEX) $(LATEXMAIN) $(DOXYCONFIG)
 else
 $(LATEXBUILDDIR)/$(LATEXPDF): $(SOURCECODE) $(SOURCETEX) $(LATEXMAIN)
 endif
-# Combined project-level bibliography files with the common references
-	cat $(SOURCEBIB) $(LOCALINC)/shared.bib > $(LATEXBUILDDIR)/$(LATEXBIB)
-ifeq ($(GENERATEDOXYGEN), YES)
-endif
-	cp -f $(SOURCETEX) $(LATEXBUILDDIR)
-	cp -f $(SOURCEIMAGES) $(LATEXBUILDDIR)
-ifeq ($(GENERATEGLOSSARY), YES)
-	cp -f $(GLOSSARY) $(LATEXBUILDDIR)
-	ln -s $(GLOSSARY) $(LATEXBUILDDIR)/$(basename $GLOSSARY).glo
-	cp $(LOCALINC)/latexmkrc $(LATEXBUILDDIR)
-endif
-ifeq ($(GENERATEDOXYGEN), YES)
-# Copy project bibliography into refman bib
-	ln -s $(LATEXBIB) $(LATEXBUILDDIR)/$(SOURCEBIB)
-# Create body links to doxygen content
-	$(foreach texdoc, $(SOURCETEX), perl -pe 's|(\\.*?doxyref){(.*?)}({([1-9]*?)})?|`scripts/$$1 $$2 $$4`|ge' -i $(LATEXBUILDDIR)/$(texdoc);)
-# Generate doxygen content
-	$(DOXYGEN) $(DOXYCONFIG)
-endif
 	cd $(LATEXBUILDDIR) && $(LATEXMK) $(LATEXROOTNAME)
+
+%.dox: build
+# Rename main TeX file as refman for doxygen
+	cp $(ROOTNAME).tex $(LATEXBUILDDIR)/$(LATEXROOTNAME).tex
+	chmod +w $(LATEXBUILDDIR)/$(LATEXROOTNAME).tex
+	$(foreach texdoc, $(SOURCETEX), perl -pe 's|(\\.*?doxyref){(.*?)}({([1-9]*?)})?|`scripts/$$1 $$2 $$4`|ge' -i $(LATEXBUILDDIR)/$(texdoc);)
+	cp $@ $(LATEXBUILDDIR)
+	$(DOXYGEN) $@
 
 clean:
 ifeq ($(shell test -f $(LATEXBUILDDIR) && echo "YES" || echo "NO"), YES)
