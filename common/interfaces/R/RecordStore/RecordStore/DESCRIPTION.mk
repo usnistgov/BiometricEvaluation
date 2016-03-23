f8706e8ec0951e2f963f288311337cbcb5b36843
Greg Fiumara <gregory.fiumara@nist.gov>
2016-03-23 12:09:01 -0500

    Updating Rcpp version to match BRL's installed version.
    [git-p4: depot-paths = "//depot/projects/EVALUATION/Main/": change = 7679]

--- common/interfaces/R/RecordStore/RecordStore/DESCRIPTION.mk
+++ common/interfaces/R/RecordStore/RecordStore/DESCRIPTION.mk
@@ -5,5 +5,5 @@ Author: NIST Image Group
 Maintainer: NIST Image Group <beframework@nist.gov>
 Description: RecordStore provides an interface (currently read-only) to RecordStores from the NIST Biometric Evaluation Framework.
 License: Unlimited
-Imports: Rcpp (>= 0.11.6)
+Imports: Rcpp (>= 0.12.3)
 LinkingTo: Rcpp
