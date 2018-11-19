11bcc706f8ae97a86ed739aa229e178b82d48e83
Gregory Fiumara <gregory.fiumara@nist.gov>
2018-11-19 15:46:43 -0500

    Remove libbiomeval.

--- common/interfaces/R/RecordStore/RecordStore/DESCRIPTION.mk
+++
@@ -1,9 +0,0 @@
-Package: RecordStore
-Type: Package
-Title: Interface to NIST Biometric Evaluation framework RecordStores.
-Author: NIST Image Group
-Maintainer: NIST Image Group <beframework@nist.gov>
-Description: RecordStore provides an interface (currently read-only) to RecordStores from the NIST Biometric Evaluation Framework.
-License: Unlimited
-Imports: Rcpp (>= 0.12.3)
-LinkingTo: Rcpp
